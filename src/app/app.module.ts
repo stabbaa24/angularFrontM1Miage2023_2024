491e246151e873f966725d3883450aeee21fc923
stabbaa <siham.tabbaa@etu.unice.fr>
2023-10-06 16:32:42 +0200

    TD1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { RenduDirective } from './shared/rendu.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent,
+    RenduDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/assignments/assignments.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Assignment } from './assignment.model';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css']
+})
+export class AssignmentsComponent implements OnInit {
+  titre = "Mon application sur les Assignments";
+  nom = "";
+  dateDeRendu = null;
+  rendu = false;
+  constructor() { }
+
+  assignments:Assignment[] = [
+    {
+      nom: 'Devoir Angular de Buffa',
+      dateDeRendu: new Date('2023-09-30'),
+      rendu: false,
+    },
+    {
+      nom: 'Devoir SQL de Mopolo',
+      dateDeRendu: new Date('2023-10-30'),
+      rendu: false,
+    },
+    {
+      nom: 'Devoir gestion de Tunsi',
+      dateDeRendu: new Date('2023-08-30'),
+      rendu: true,
+    },
+  ];
+  ngOnInit(): void {
+    
+  }
+
+}
